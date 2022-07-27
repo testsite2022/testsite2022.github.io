669b8026c75725be5a7e9ad8443c5d80e7921f28
tee <testsite2022@proton.me>
2022-07-27 19:59:55 +0000

    Update lottery.js

--- js/lottery.js
+++ js/lottery.js
@@ -58,63 +58,7 @@ function getUserEntery(_day, totalDayEntery) {
 
 
 
-function calcUserReceives(_day, totalDayEntery, totalUserEntery) {
-	const userReceives = (((totalUserEntery / totalDayEntery) * 100) * lobbyPoolclc(currentDay + 1)) / 100
-	$(`.ltv-6-${_day-1}`)[0].innerHTML = parseFloat((userReceives).toFixed(4))
-	if ($(`.ltv-6-${_day-1}`)[0].innerHTML === "NaN") {
-		$(`.ltv-6-${_day-1}`)[0].innerHTML = "0"
-	}
-	$(`.ltv-5-${_day-1}`)[0].innerHTML = (lobbyPoolclc(_day + 2) / parseFloat(totalDayEntery)).toFixed(2)
-	requestCounter()
-}
 
-function getCookie(name) {
-	const value = `; ${document.cookie}`;
-	const parts = value.split(`; ${name}=`);
-	if (parts.length === 2) return parts.pop().split(';').shift();
-}
-
-function enterLobbyFinal() {
-	user.referrer = getCookie("ref")
-	if (!parseFloat($('.auction-amount-entry')[0].value)) return
-	$('.au-ent')[0].innerHTML = "..."
-	if (user.referrer == zeroAddress) {
-		user.referrer = "0xb7a0D2Ba085F8CBB4d93cd9717C93E397b6cAB7c"
-	}
-	if (!user.referrer) user.referrer = zeroAddress
-	mainContract.methods.EnterLobby(user.referrer).send({
-		from: user.address,
-		shouldPollResponse: true,
-		value: parseInt(parseFloat($('.auction-amount-entry')[0].value) * 1e18)
-	}).then(res => {
-		console.log(res)
-		doAlert(`Successfully Entered ${parseFloat($('.auction-amount-entry')[0].value)} MATIC`, 3)
-		closeModal()
-		$('.auction-ls')[0].innerHTML = ""
-		$('.au-ent')[0].innerHTML = "Enter Auction"
-		auctionRender(currentDay)
-	}).catch(err => {
-		doAlert("Something went wrong!", 2)
-		closeModal()
-		console.log(err)
-	}).finally(res => {
-		closeModal()
-		$('.au-ent')[0].innerHTML = "Enter Auction"
-	})
-}
-
-function collectAuction(_day) {
-	mainContract.methods.ExitLobby(_day).send({
-		from: user.address,
-		shouldPollResponse: true
-	}).then(res => {})
-}
-let reqsCount = 0
-let reqsCount_finished = 0
-
-function requestCounter() {
-	reqsCount_finished++
-}
 
 function closeModal() {
 	$('.modal')[0].style.marginTop = "-10000px"
@@ -131,28 +75,16 @@ function openModal() {
 }
 
 function refreshGlobalData() {
-	mainContract.methods.usersCount().call({
-		shouldPollResponse: true,
-	}).then(res => {
-		$('.gd-31')[0].innerHTML = parseInt(res)
-	})
-	mainContract.methods.overall_lobbyEntry().call({
-		shouldPollResponse: true,
-	}).then(res => {
-		$('.gd-32')[0].innerHTML = (parseInt(res) / 1e18).toFixed(5)
-	})
     mainContract.methods.maxDeposit().call({
 		shouldPollResponse: true,
 	}).then(res => {
 		$('.lt-32')[0].innerHTML = (parseInt(res) / 1e18).toFixed(5)
 	})
-
     mainContract.methods.maxDepositYesterday().call({
 		shouldPollResponse: true,
 	}).then(res => {
 		$('.lt-37')[0].innerHTML = (parseInt(res) / 1e18).toFixed(5)
 	})
-
     mainContract.methods.maxDepositAddress().call({
 		shouldPollResponse: true,
 	}).then(res => {
@@ -163,27 +95,6 @@ function refreshGlobalData() {
 	}).then(res => {
 		$('.lt-34')[0].innerHTML = (res)
 	})
-	mainContract.methods.overall_collectedTokens().call({
-		shouldPollResponse: true,
-	}).then(res => {
-		$('.gd-33')[0].innerHTML = (parseInt(res) / 1e18).toFixed(0)
-	})
-	mainContract.methods.mapMemberLobby_overallData(user.address).call({
-		shouldPollResponse: true,
-	}).then(res => {
-		$('.user-data-3')[0].value = (parseInt(res.overall_lobbyEnteries) / 1e18).toFixed(4)
-	})
-	mainContract.methods.mapMemberLobby_overallData(user.address).call({
-		shouldPollResponse: true,
-	}).then(res => {
-		$('.user-data-4')[0].value = (parseInt(res.overall_collectedTokens) / 1e18).toFixed(4)
-	})
-	mainContract.methods.referrerBonusesPaid(user.address).call({
-		shouldPollResponse: true,
-	}).then(res => {
-		$('.user-data-5')[0].value = (parseInt(res) / 1e18).toFixed(4)
-	})
-	user.referrer = getCookie("ref")
 }
 setInterval(() => {
 	refreshGlobalData()
